dac786d9d13c865b05c942e3be5c6a27df5c42b4
Darshana Perera <91814341+darshana-perera-97@users.noreply.github.com>
2022-11-09 10:29:34 +0530

    start pattern 33 added

---
+++ startPatern3.asm
@@ -0,0 +1,18 @@
+; 5 5 5 5 5 
+
+
+
+section	.data
+
+i times 10 db '5 '	
+
+section	.text
+	global _start       
+_start:                   
+    mov edx,10
+    mov ecx,i
+    mov eax,4
+	int	0x80      
+	mov	eax, 1	   
+	int	0x80       
+
